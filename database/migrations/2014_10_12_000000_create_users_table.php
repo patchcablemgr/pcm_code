19c5cd08fd037ff021a49c6431c28fe9e523a452
Garrett <garrett.kelly@patchcablemgr.com>
2022-05-01 04:37:25 +0000

    user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+						$table->boolean('status');
+						$table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });

f486e4b67e7d6383b58db5425a3923d6d7183e5a
Garrett <garrett.kelly88@gmail.com>
2022-06-18 12:59:00 +0000

    pcm_code

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -12,24 +12,24 @@
 @endstory
  
 @task('update-code')
-    cd /home/garrett/pcm_infra/laravel
+    cd /home/garrett/pcm_selfhosted/app
     git pull
 @endtask
  
 @task('install-dependencies')
-    cd /home/garrett/pcm_infra/laravel
+    cd /home/garrett/pcm_selfhosted/app
     composer install
     npm install
-    cd /home/garrett/pcm_infra/laravel/frontend
+    cd /home/garrett/pcm_selfhosted/app/frontend
     npm install
 @endtask
 
 @task('compile')
-    cd /home/garrett/pcm_infra/laravel
+    cd /home/garrett/pcm_selfhosted/app
     npm run prod
 @endtask
 
 @task('run-migrations')
-    cd /home/garrett/pcm_infra/laravel/vendor/bin
+    cd /home/garrett/pcm_selfhosted/app/vendor/bin
     sail artisan migrate
 @endtask

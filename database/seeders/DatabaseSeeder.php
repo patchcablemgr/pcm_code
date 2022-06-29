51e5e26c235007120b5568a4a4426d3eaa99e12e
Garrett <garrett.kelly88@gmail.com>
2022-06-29 20:15:21 +0000

    pcm_code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -178,7 +178,7 @@ class DatabaseSeeder extends Seeder
         }
 
         // tenant
-        $appID = Str::random(10);
+        $appID = guidv4();
         $version = getenv('APP_VERSION');
         $licenseData = [
             'status' => 'inactive',
@@ -193,5 +193,25 @@ class DatabaseSeeder extends Seeder
         $tenant = [ 'name' => 'Acme', 'license_last_checked' => time(), 'license_data' => $licenseData, 'app_id' => $appID, 'version' => $version];
 
         DB::table('tenant')->insert($tenant);
+    }
+
+    /**
+     * Generate UUID
+     *
+     * @return str
+     */
+    public function guidv4()
+    {
+        // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+        $data = random_bytes(16);
+        assert(strlen($data) == 16);
+
+        // Set version to 0100
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        // Set bits 6-7 to 10
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+        // Output the 36 character UUID.
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 }

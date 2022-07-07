a03f6fa82e70c3f00ff9b476911f60ea5c580530
Garrett <garrett.kelly88@gmail.com>
2022-07-07 13:56:23 +0000

    pcm_code

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -9,27 +9,33 @@
     install-dependencies
     compile
     run-migrations
+    run-seeder
 @endstory
  
 @task('update-code')
-    cd /home/garrett/pcm_selfhosted/app
+    cd /home/pcmuser/pcm_selfhosted/app
     git pull
 @endtask
  
 @task('install-dependencies')
-    cd /home/garrett/pcm_selfhosted/app
+    cd /home/pcmuser/pcm_selfhosted/app
     composer install
     npm install
-    cd /home/garrett/pcm_selfhosted/app/frontend
+    cd /home/pcmuser/pcm_selfhosted/app/frontend
     npm install
 @endtask
 
 @task('compile')
-    cd /home/garrett/pcm_selfhosted/app
+    cd /home/pcmuser/pcm_selfhosted/app
     npm run prod
 @endtask
 
 @task('run-migrations')
-    cd /home/garrett/pcm_selfhosted/app/vendor/bin
-    sail artisan migrate
+    cd /home/pcmuser/pcm_selfhosted/app/vendor/bin
+    sudo sail artisan migrate
+@endtask
+
+@task('run-seeder')
+    cd /home/pcmuser/pcm_selfhosted/app/vendor/bin
+    sudo sail artisan db:seed
 @endtask

bf039bf353a8317a7793141aa468b52530e6a630
Garrett <garrett.kelly88@gmail.com>
2022-07-09 01:54:18 +0000

    pcm_code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 use App\Models\OrganizationModel;
 use Database\Seeders\Seeder_1_0_0;
 use Database\Seeders\Seeder_1_0_1;
+use Illuminate\Support\Facades\Log;
 
 class DatabaseSeeder extends Seeder
 {
@@ -25,9 +26,10 @@ class DatabaseSeeder extends Seeder
 
         $organization = DB::table('organization')->first();
         $currentVersion = $organization->version;
+        Log::info($currentVersion);
 
         // 1.0.0 -> 1.0.1
-        if(version_compare($currentVersion, '1.0.0') == 0) {
+        if(version_compare($currentVersion, '1.0.0') === 0) {
             $this->call([Seeder_1_0_1::class]);
         }
 

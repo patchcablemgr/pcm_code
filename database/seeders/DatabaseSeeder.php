e49f7a6ad56c76289384ccce756e9ec1fa9cbe70
Garrett <garrett.kelly88@gmail.com>
2022-08-23 13:11:01 +0000

    pcm_code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -48,5 +48,13 @@ class DatabaseSeeder extends Seeder
             $org->save();
         }
 
+        $orgVersion = $org->version;
+
+        // 1.1.0 -> 1.2.0
+        if(version_compare($orgVersion, '1.1.0') === 0) {
+            $org->version = '1.2.0';
+            $org->save();
+        }
+
     }
 }
